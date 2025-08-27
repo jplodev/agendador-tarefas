81bba142d4d4858765f40b581a9b61d71d880602
João Paulo Lima <jplopapa.dev@gmail.com>
2025-08-26 21:00:00 -0300

    Commit inicial com esqueleto

---
+++ src/test/java/com/jpdev/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdev.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
