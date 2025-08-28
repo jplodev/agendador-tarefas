4250442cac0b85608d6f0da5ea8a4d2f139ec0f9
João Paulo Lima <jplopapa.dev@gmail.com>
2025-08-28 20:20:31 -0300

    Adicionado método Post

---
+++ src/main/java/com/jpdev/agendadortarefas/controller/TarefasController.java
@@ -0,0 +1,21 @@
+package com.jpdev.agendadortarefas.controller;
+
+import com.jpdev.agendadortarefas.business.TarefasService;
+import com.jpdev.agendadortarefas.business.dto.TarefasDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/tarefas")
+public class TarefasController {
+
+    private final TarefasService tarefasService;
+
+    @PostMapping
+    public ResponseEntity<TarefasDTO> gravarTarefas(@RequestBody TarefasDTO dto,
+                                                    @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(tarefasService.gravarTarefa(token, dto));
+    }
+}
