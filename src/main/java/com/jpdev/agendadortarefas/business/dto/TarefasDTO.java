0029cc985c9eb8fb7fda68634e6d23943f179442
João Paulo Lima <jplopapa.dev@gmail.com>
2026-07-26 19:49:38 -0300

    Adicionadas configurações de Banco de Dados

--- src/main/java/com/jpdev/agendadortarefas/business/dto/TarefasDTO.java
+++ src/main/java/com/jpdev/agendadortarefas/business/dto/TarefasDTO.java
@@ -1,5 +1,6 @@
 package com.jpdev.agendadortarefas.business.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jpdev.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import lombok.*;
 
@@ -16,8 +17,11 @@ public class TarefasDTO {
     private String nomeTarefa;
     private String descricao;
     private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dataEvento;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dataAlteracao;
     private StatusNotificacaoEnum statusNotificacaoEnum;
 }
